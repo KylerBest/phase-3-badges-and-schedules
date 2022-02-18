8edd875364f9ba2bff918d09220a0c25a283cfd3
Kyler Best <kyler.b@live.com>
2022-02-18 17:58:35 -0600

    commit tree 6e8bead30b9ff00a922c85d9a32cbc0f4efe4400

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+def badge_maker name
+    "Hello, my name is #{name}."
+end
+def batch_badge_creator names
+    names.map {|name| badge_maker(name)}
+end
+def assign_rooms names
+    names.map.with_index(1) {|name, i| "Hello, #{name}! You'll be assigned to room #{i}!"}
+end
+def printer names 
+    batch_badge_creator(names).each {|badge| puts badge}
+    assign_rooms(names).each {|assignment| puts assignment}
+end
